ad3d4a10219602e39b786babf3acbacd7d5e906b
Micael Teixeira <mikatx2@hotmail.com>
2022-03-10 13:59:44 +0100

    Update Program.cs

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ArrayBiDim
 {
@@ -6,7 +6,34 @@ namespace ArrayBiDim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dhoriz; 
+            int vhoriz;
+            int i=0;
+            int j=0;
+
+            Console.WriteLine("insira a dimensao horizontal e vertical do array");
+            dhoriz = Int32.Parse(Console.ReadLine());
+            vhoriz = Int32.Parse(Console.ReadLine());
+
+            float[ , ] arrayX = new float[dhoriz, vhoriz];
+
+            for(i=0; i<vhoriz; i++){
+
+                for(j=0; j<dhoriz; j++){
+                    Console.Write($"Insira Linha {i} e coluna {j}->");
+                    arrayX[i, j] = float.Parse(Console.ReadLine());
+                }
+            }
+
+            //print array
+            for(i=0; i<vhoriz; i++){
+                for(j=0; j<dhoriz; j++){
+                    Console.Write($"{arrayX[i, j]} | ");
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
