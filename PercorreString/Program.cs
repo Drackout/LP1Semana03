fe382f218d9614cedfac0e385860eae0d5bd4a22
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-03-10 12:08:10 +0100

    one commit on solution

---
+++ PercorreString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringPedida;
+            char charPedida;
+            Console.Write("String->");
+            stringPedida = Console.ReadLine();
+
+            Console.Write("Char->");
+            charPedida = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"String inteira: {stringPedida}");
+            Console.WriteLine($"Char: {charPedida}");
+
+            Console.WriteLine("String sem char");
+        }
+    }
+}
