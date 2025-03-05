960c0da3c2e5695beab25b0fb0687e9924fba19b
Drackout <mikatx2@hotmail.com>
2025-03-05 19:08:48 +0000

    Exercicio 1

---
+++ RightSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string item in args)
+            {
+                if (item.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                if(item.Length > 3)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
