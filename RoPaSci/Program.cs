6f37d3b9cb3a456201841a519267d8513604f4a2
Drackout <mikatx2@hotmail.com>
2025-03-06 00:03:42 +0000

    Ex8 changes

--- RoPaSci/Program.cs
+++ RoPaSci/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Reflection;
 
 namespace RoPaSci
 {
-    public class Program
+    class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int result = RockPaperScissors(args[0], args[1]);
+            switch (result)
+            {
+                case 0:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case 1:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case 2:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+        private static int RockPaperScissors(string player1, string player2)
+        {
+            int winner;
+            if (player1 == player2)
+            {
+                winner = 0; // Draw
+            }
+            if (((player1 == "Rock") && (player2 == "Scissors")) ||
+                ((player1 == "Scissors") && (player2 == "Paper")) ||
+                ((player1 == "Paper") && (player2 == "Rock")))
+            {
+                winner = 1; // Player 1 wins
+            }
+            else
+            {
+                winner = 2; // Player 2 wins
+            }
+            return winner;
         }
     }
 }
